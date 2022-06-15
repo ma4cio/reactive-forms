201b67400d0182e00f2891d5b3b373d2e7ff9a28
Marcio Lima <marcio.lima@bancodaycoval.com.br>
2022-06-15 00:15:43 -0300

    Criacao e utilizacao dos formularios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Componets
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+    path: '',  
+    component: ReactiveFormsComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
