3acd1d2766ab1265a3ed00576cf969023de3cee0
Marcio Lima <ma4cio@gmail.com>
2022-06-16 13:33:04 -0300

    Valid e Invalid

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -19,8 +19,11 @@ export class ReactiveFormsComponent implements OnInit {
   }
 
   public submitForm(){
-    console.log(this.cadastroForm.value);
-    console.log(this.cadastroForm.value.firstName);
+    if(this.cadastroForm.valid){
+      console.log(this.cadastroForm.value);
+      console.log(this.cadastroForm.value);
+      console.log(this.cadastroForm.value.firstName);
+    }
   }
 
 }
