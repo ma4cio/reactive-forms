23badd5696487ef6213bdab70e026aebe2ddcd93
Marcio Lima <ma4cio@gmail.com>
2022-06-16 14:33:10 -0300

    Entendo sobre validators

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -10,7 +10,7 @@ export class ReactiveFormsComponent implements OnInit {
 
   public cadastroForm: FormGroup = this.formBuilder.group({
       firstName:['', Validators.required],
-      lastName:['']
+      lastName:['', [Validators.required, Validators.minLength(5)]]
   })
 
   constructor(private formBuilder: FormBuilder) { }
